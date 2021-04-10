27a7be7067edbd3c694923dc5cf6085bd3738252
Aderock13 <81279279+Aderock13@users.noreply.github.com>
2021-04-10 18:51:04 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAKgRkeoUSZAOQ38fEiGOeDadnSYL-QXX0",
+    authDomain: "autentificamictlan.firebaseapp.com",
+    projectId: "autentificamictlan",
+    storageBucket: "autentificamictlan.appspot.com",
+    messagingSenderId: "444433483910",
+    appId: "1:444433483910:web:c8fe2125069d7ab6b3e907",
+    measurementId: "G-15T0CRCE7V"
+    });
