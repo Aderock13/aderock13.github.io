059680b41fdf886d18eacc42fc9c83f99c6c2dd0
Aderock13 <81279279+Aderock13@users.noreply.github.com>
2021-04-13 18:22:13 -0500

    Create init2.js

---
+++ js/init2.js
@@ -0,0 +1,7 @@
+  apiKey: "AIzaSyDcnRWs8xaw-vGZ1Q1rnpUd1jo0aZREEeg",
+    authDomain: "chatmictlan.firebaseapp.com",
+    projectId: "chatmictlan",
+    storageBucket: "chatmictlan.appspot.com",
+    messagingSenderId: "422630460836",
+    appId: "1:422630460836:web:c30dcc0090154de3e9c531",
+    measurementId: "G-BC3H7BXFE6"
